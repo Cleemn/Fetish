0eb7664fe89fb5187936e7ad9586393457d3ae2a
Bertrand Lamache <bertrand.lamache@free.fr>
2019-08-26 16:35:47 +0200

    Columns added Users

---
+++ db/migrate/20190826140147_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :user_name, :string
+    add_column :users, :avatar, :string
+    add_column :users, :description, :text
+    add_column :users, :admin, :boolean
+    add_column :users, :birth_date, :date
+    add_column :users, :post_code, :string
+  end
+end
