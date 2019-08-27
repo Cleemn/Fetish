28fbb8b7f0036698c06398078f0fea2937279e7d
Mathilde Louradour <mathilde.louradour@gmail.com>
2019-08-27 16:37:14 +0200

    random page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def random
+    # @user_random = []
+    # @user_random = User.where(fetishes: current_user.fetishes).order("RANDOM()").first
     @user = User.order("RANDOM()").first
    # @user = User.where"#{current_user.fetishes} LIKE ?", "%#{@fetishes}%".order("RANDOM()").first
   end
