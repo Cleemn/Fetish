0816cdbaf8a0515e2bbdafb1be13dff57095fac4
mariecaroline meijer <meijer.marie.caroline@gmail.com>
2019-08-30 12:09:20 +0200

    action cable workiiiiing

--- app/javascript/plugins/init_action_cable.js
+++ app/javascript/plugins/init_action_cable.js
@@ -1,3 +1,5 @@
+import { scrollLastMessageIntoView } from './scroll.js';
+
 const initActionCable = () => {
   const connect = document.getElementById('connect-action-cable');
   if (connect) {
@@ -6,10 +8,14 @@ const initActionCable = () => {
       { channel: 'MatchesChatChannel', match_id: matchId },
       { received: (data) => {
         // select messages list
-        // const MyChatListId = document.getElementById('my-chat-list');
-        // MyChatListId.insertAdjacentHTML("beforeend", data.message_partial);
-        // document.getElementById('message_content').value = "";
-        // scrollLastMessageIntoView();
+        const currentUserId = parseInt(connect.dataset.currentUserId, 10);
+        if (data.message_user_id !== currentUserId) {
+          const MyChatListId = document.getElementById('my-chat-list');
+          MyChatListId.insertAdjacentHTML("beforeend", data.message_partial);
+          document.getElementById('message_content').value = "";
+          scrollLastMessageIntoView();
+        }
+
         // insert new message at the end of the list
         // console.log(data.message_partial)
       } }
