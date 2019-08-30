044eddeee8ede4d30315b9133b83d7ca8eb0cecd
mariecaroline meijer <meijer.marie.caroline@gmail.com>
2019-08-30 11:36:45 +0200

    almost working:

--- app/javascript/plugins/init_action_cable.js
+++ app/javascript/plugins/init_action_cable.js
@@ -6,9 +6,9 @@ const initActionCable = () => {
       { channel: 'MatchesChatChannel', match_id: matchId },
       { received: (data) => {
         // select messages list
-        const MyChatListId = document.getElementById('my-chat-list');
-        MyChatListId.insertAdjacentHTML("beforeend", data.message_partial);
-        document.getElementById('message_content').value = "";
+        // const MyChatListId = document.getElementById('my-chat-list');
+        // MyChatListId.insertAdjacentHTML("beforeend", data.message_partial);
+        // document.getElementById('message_content').value = "";
         // scrollLastMessageIntoView();
         // insert new message at the end of the list
         // console.log(data.message_partial)
@@ -20,3 +20,6 @@ const initActionCable = () => {
 export { initActionCable };
 
 // beug de front a fixer apres
+// notre formulaire ne revient pas a blank
+// le message ne semble pas s'envoyer alors qu'il s'envoie de l'autre cote
+// mettre en place correctement le scroll
