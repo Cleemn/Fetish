98c8816171c6dfda158b655899815e783af096eb
mariecaroline meijer <meijer.marie.caroline@gmail.com>
2019-09-02 14:48:50 +0200

    Everything is better now

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,23 +2,12 @@ class MatchesController < ApplicationController
   def index
     @matches = Match.where(user_1_id: current_user.id).or(Match.where(user_2_id: current_user.id))
   end
-  # puis-je tej cette methode? ainsi que route, action controller
-
 
   def new
     @match = Match.new
     @user = User.find(params[:user_id])
     @match.user = @user
   end
-
-
-  # def new
-  #   @match = Match.new
-  #   @user = User.find(params[:id])
-  #   # quel id ici?
-  #   @match.user = @user
-  # end
-
 
   def show
     @match = Match.find(params[:id])
@@ -40,23 +29,7 @@ class MatchesController < ApplicationController
     else
       render :new
     end
-
   end
-
-  # def show
-  #   @match = Match.find(params[:match_id])
-  #   @user = User.find(:match_id)
-  #   raise
-  # end
-
-  # def checked
-  #   @match =
-  #   @new_notifications = @match.messages.where(newnotification: true)
-  #   @new_notifications.each do |new_notif|
-  #     new_notif.newnotification = false
-  #     new_notif.save
-  #   end
-  # end
 
   def accepted
     @match = Match.find(params[:match_id])
