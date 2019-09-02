852f1f9cd9322d7af92dbec8229826a92cc64940
Bertrand LAMACHE <45500565+blamache@users.noreply.github.com>
2019-09-02 10:46:02 +0200

    Delete seeds_tmp_sv.rb

--- db/migrate/seeds_tmp_sv.rb
+++
@@ -1,3 +0,0 @@
-        <i class="fas fa-exclamation-triangle"></i>
-        <%= link_to image_tag(class="fas fa-exclamation-triangle"), match_end_path(match_id: @match.id), :method => :post %>
-        user_reports_path(user_id: @user.id)
